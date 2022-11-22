ece38770e1a2bcf819e263ac7e7f448b0164d8b7
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-11-22 23:56:29 +0530

    Add New Example

---
+++ src/Secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "Unknown"
+#define SECRET_PASS "aabbccdd"
+#define MQTT_USERNAME "gwbvwhzr:gwbvwhzr"
+#define MQTT_PASS "BH4UyDm74GHbzdsYJOFtvZL7LTIM_bNB"
--- src/sketch.ino
+++ src/sketch.ino
@@ -1,71 +1,136 @@
+#include <ArduinoMqttClient.h>
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+  #include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+  #include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+  #include <ESP8266WiFi.h>
+#elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) || defined(ARDUINO_ARCH_ESP32)
+  #include <WiFi.h>
+#endif
+
 #include "SimpleWifi.h"
-#include "SimpleOta.h"
-#include <ESP8266HTTPClient.h>
 #include "JsonMapper.h"
 
-#define INTERVAL 1000
+#define BIN_URL_RECEIVED "BIN_URL_RECEIVED"
+#define BIN_DOWNLOADING "BIN_DOWNLOADING"
+#define BIN_DOWNLOADED "BIN_DOWNLOADED"
+#define BIN_DOWNLOAD_FAILED "BIN_DOWNLOAD_FAILED"
+#define UPDATING "UPDATING"
+#define UPDATED "UPDATED"
+#define UPDATE_FAILED "UPDATE_FAILED"
+#define RESTARTING "RESTARTING"
+#define STARTED "STARTED"
+
+#include "Secrets.h"
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+char *wifiSSID = SECRET_SSID;
+char *wifiPassword = SECRET_PASS;
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "puffin.rmq2.cloudamqp.com";
+int port = 1883;
+
+const char topic[]  = "main/update";
+
+const long interval = 1000;
 unsigned long previousMillis = 0;
-Json greeting;
 
-char *wifiSSID = "GuitarZeed_WiFi";
-char *wifiPassword = "guitarzeed999";
-HTTPClient http;
-WiFiClient wiFiClient;
-
-// MQTT
-#include <PubSubClient.h>
-PubSubClient client(wiFiClient);
-String topic = "main/update";
-const char* mqttServer = "woodpecker.rmq.cloudamqp.com";
-int mqttPort = 1883;
-const char* mqttUsername = "dahrusvc:dahrusvc";
-const char* mqttPassword = "6lYM_XMYbBTM-rfU3vg4Qsdfmx8J1TlA";
-const char* mqttClientId = "ESP8266Client";
-
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect(mqttClientId, mqttUsername, mqttPassword)) {
-      Serial.println("Connected");
-      client.publish(topic.c_str(), greeting.getJson().c_str());
-      client.subscribe(topic.c_str());
-    } else {
-      Serial.print("Failed, rc = ");
-      Serial.print(client.state());
-      Serial.println(", try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
-
-void mqttPayloadProcess(char* mqttTopic, byte* mqttPayload, unsigned int mqttPayloadLength) {
-  Json json;
-  json.setJson(String((char*) mqttPayload));
-  String jsonString = json.getJson();
-  Serial.print("Parsed json: ");
-  Serial.println(jsonString);
-  String url = getJsonStringProperty(jsonString, "url");
-  if (getJsonBooleanProperty(jsonString, "update") && url.length() > 0) {
-    initializeOta(wiFiClient, wifiSSID, wifiPassword, url);
-  }
-}
+int count = 0;
 
 void setup() {
   Serial.begin(115200);
-  greeting.addStringProperty("message", "V0");
+  // while (!Serial) {
+  //   ; // wait for serial port to connect. Needed for native USB port only
+  // }
   connectWifi(wifiSSID, wifiPassword);
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(mqttPayloadProcess);
+  checkInternet(wifiClient, wifiSSID, wifiPassword);
+
+  mqttClient.setId("clientId");
+  mqttClient.setUsernamePassword(MQTT_USERNAME, MQTT_PASS);
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
+
+  Serial.print("Subscribing to topic: ");
+  Serial.println(topic);
+  Serial.println();
+
+  // subscribe to a topic
+  mqttClient.subscribe(topic);
+
+  // topics can be unsubscribed using:
+  // mqttClient.unsubscribe(topic);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  // check for incoming messages
+  int messageSize = mqttClient.parseMessage();
+  if (messageSize && String(mqttClient.messageTopic()) == String(topic)) {
+    String message = "";
+    while (mqttClient.available()) {
+      char currentChar = (char)mqttClient.read();
+      message.concat(currentChar);
+    }
+    Json jsonMessage;
+    jsonMessage.setJson(message);
+    String jsonMessageString = jsonMessage.getJson();
+    Serial.print("Parsed json: ");
+    Serial.println(jsonMessageString);
+    String url = getJsonStringProperty(jsonMessageString, "url");
+    if (url != "") {
+      Json response;
+      response.cloneProperty("id", jsonMessageString);
+      response.cloneProperty("commit", jsonMessageString);
+      response.addStringProperty("stage", BIN_URL_RECEIVED);
+      mqttClient.beginMessage(topic);
+      mqttClient.print(response.getJson());
+      mqttClient.endMessage();
+      // mock up
+      delay(1000);
+      response.patchStringProperty("stage", BIN_DOWNLOADING);
+      mqttClient.beginMessage(topic);
+      mqttClient.print(response.getJson());
+      mqttClient.endMessage();
+      delay(1000);
+      response.patchStringProperty("stage", BIN_DOWNLOADED);
+      mqttClient.beginMessage(topic);
+      mqttClient.print(response.getJson());
+      mqttClient.endMessage();
+      delay(1000);
+      response.patchStringProperty("stage", UPDATING);
+      mqttClient.beginMessage(topic);
+      mqttClient.print(response.getJson());
+      mqttClient.endMessage();
+      delay(1000);
+      response.patchStringProperty("stage", UPDATED);
+      mqttClient.beginMessage(topic);
+      mqttClient.print(response.getJson());
+      mqttClient.endMessage();
+      delay(1000);
+      response.patchStringProperty("stage", RESTARTING);
+      mqttClient.beginMessage(topic);
+      mqttClient.print(response.getJson());
+      mqttClient.endMessage();
+      delay(1000);
+      response.patchStringProperty("stage", STARTED);
+      mqttClient.beginMessage(topic);
+      mqttClient.print(response.getJson());
+      mqttClient.endMessage();
+    }
   }
-  client.loop();
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis > INTERVAL) {
-    previousMillis = currentMillis;
-    Serial.println(greeting.getJson());
-  }
+
+  // do something else
 }
