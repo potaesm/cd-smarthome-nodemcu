7ef892743dda2303666a76224fd6eecd48b8a74a
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-12-05 23:03:42 +0530

    New version

--- src/sketch.ino
+++ src/sketch.ino
@@ -22,9 +22,7 @@
 #define NO_UPDATES "NO_UPDATES"
 #define UPDATE_OK "UPDATE_OK"
 
-#include "Secrets.h"
-char ssid[] = SECRET_SSID;
-char pass[] = SECRET_PASS;
+#include "arduino_secrets.h"
 char *wifiSSID = SECRET_SSID;
 char *wifiPassword = SECRET_PASS;
 
@@ -35,27 +33,91 @@ const char broker[] = "puffin.rmq2.cloudamqp.com";
 int port = 1883;
 bool retained = false;
 int qos = 2;
-bool dup = false;
 const char topic[]  = "main/update";
 bool blinkStatus = false;
-const long interval = 250;
+const long interval = 1000;
 
 unsigned long previousMillis = 0;
 
-void update_started() {
-  Serial.println("CALLBACK:  HTTP update process started");
+void handleESP8266Update();
+void handleMQTTMessage(void (*)());
+
+String id = "";
+String commit = "";
+String url = "";
+
+void handleMQTTMessage(void (*function)()){
+  if (mqttClient.parseMessage() && String(mqttClient.messageTopic()) == String(topic)) {
+    String message = "";
+    while (mqttClient.available()) {
+      char currentChar = (char)mqttClient.read();
+      message.concat(currentChar);
+    }
+    Json jsonMessage;
+    jsonMessage.setJson(message);
+    Serial.println("Parsed message: " + jsonMessage.getJson());
+    id = getJsonStringProperty(jsonMessage.getJson(), "id");
+    commit = getJsonStringProperty(jsonMessage.getJson(), "commit");
+    url = getJsonStringProperty(jsonMessage.getJson(), "url");
+    bool isAllowed = (id == "") ? true : (id == DEVICE_ID) ? true : false;
+    if (url != "" && isAllowed) {
+      (*function)();
+    }
+  }
 }
 
-void update_finished() {
-  Serial.println("CALLBACK:  HTTP update process finished");
+void handleESP8266Update() {
+  Json response;
+  response.addStringProperty("id", DEVICE_ID);
+  response.addStringProperty("commit", commit);
+  response.addStringProperty("stage", BIN_URL_RECEIVED);
+  sendMQTTMessage(response.getJson());
+  mqttClient.stop();
+  // Start update
+  ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+  // ESPhttpUpdate.closeConnectionsOnUpdate(false);
+  ESPhttpUpdate.rebootOnUpdate(false);
+  t_httpUpdate_return ESPHttpUpdateReturn = ESPhttpUpdate.update(wifiClient, url);
+  connectMQTTBroker();
+  switch (ESPHttpUpdateReturn) {
+    case HTTP_UPDATE_FAILED: {
+        Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+        response.patchStringProperty("stage", String(UPDATE_FAILED) + " (" + String(ESPhttpUpdate.getLastError()) + "): " + ESPhttpUpdate.getLastErrorString());
+        sendMQTTMessage(response.getJson());
+        break;
+      }
+    case HTTP_UPDATE_NO_UPDATES: {
+        Serial.println("HTTP_UPDATE_NO_UPDATES");
+        response.patchStringProperty("stage", NO_UPDATES);
+        sendMQTTMessage(response.getJson());
+        break;
+      }
+    case HTTP_UPDATE_OK: {
+        Serial.println("HTTP_UPDATE_OK");
+        response.patchStringProperty("stage", UPDATE_OK);
+        sendMQTTMessage(response.getJson());
+        delay(100);
+        ESP.restart();
+        break;
+      }
+  }
 }
 
-void update_progress(int cur, int total) {
-  Serial.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+void sendMQTTMessage(String message) {
+  mqttClient.beginMessage(topic, message.length(), retained, qos);
+  mqttClient.print(message);
+  mqttClient.endMessage();
 }
 
-void update_error(int err) {
-  Serial.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+void connectMQTTBroker() {
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+    while (1);
+  }
+  Serial.println("Connected to broker " + String(broker));
+  mqttClient.subscribe(topic, qos);
+  Serial.println("Subscribed to topic " + String(topic));
 }
 
 void setup() {
@@ -66,89 +128,16 @@ void setup() {
 
   mqttClient.setId(DEVICE_ID);
   mqttClient.setUsernamePassword(MQTT_USERNAME, MQTT_PASS);
+  connectMQTTBroker();
 
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-    while (1);
-  }
-  Serial.println("MQTT connected to broker " + String(broker));
-  mqttClient.subscribe(topic, qos);
-  // topics can be unsubscribed using:
-  // mqttClient.unsubscribe(topic);
-  Serial.println("MQTT subscribed to topic " + String(topic));
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // check for incoming messages
-  int messageSize = mqttClient.parseMessage();
-  if (messageSize && String(mqttClient.messageTopic()) == String(topic)) {
-    String message = "";
-    while (mqttClient.available()) {
-      char currentChar = (char)mqttClient.read();
-      message.concat(currentChar);
-    }
-    Json jsonMessage;
-    jsonMessage.setJson(message);
-    String jsonMessageString = jsonMessage.getJson();
-    Serial.println("Parsed message: " + jsonMessageString);
-    String url = getJsonStringProperty(jsonMessageString, "url");
-    String id = getJsonStringProperty(jsonMessageString, "id");
-    bool isAllowed = (id == "") ? true : (id == DEVICE_ID) ? true : false;
-    if (url != "" && isAllowed) {
-      Json response;
-      response.addStringProperty("id", DEVICE_ID);
-      response.cloneProperty("commit", jsonMessageString);
-      response.patchStringProperty("stage", BIN_URL_RECEIVED);
-      mqttClient.beginMessage(topic, response.getJson().length(), retained, qos, dup);
-      mqttClient.print(response.getJson());
-      mqttClient.endMessage();
-      // Start update
-      ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
-      ESPhttpUpdate.rebootOnUpdate(false);
-      ESPhttpUpdate.onStart(update_started);
-      ESPhttpUpdate.onEnd(update_finished);
-      ESPhttpUpdate.onProgress(update_progress);
-      ESPhttpUpdate.onError(update_error);
-      t_httpUpdate_return ret = ESPhttpUpdate.update(wifiClient, url);
-      if (!mqttClient.connect(broker, port)) {
-        Serial.print("MQTT connection failed! Error code = ");
-        Serial.println(mqttClient.connectError());
-        while (1);
-      }
-      Serial.println("MQTT connected to broker " + String(broker));
-      switch (ret) {
-        case HTTP_UPDATE_FAILED: {
-            Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-            response.patchStringProperty("stage", String(UPDATE_FAILED) + " (" + String(ESPhttpUpdate.getLastError()) + "): " + ESPhttpUpdate.getLastErrorString());
-            mqttClient.beginMessage(topic, response.getJson().length(), retained, qos, dup);
-            mqttClient.print(response.getJson());
-            mqttClient.endMessage();
-            break;
-          }
-        case HTTP_UPDATE_NO_UPDATES: {
-            Serial.println("HTTP_UPDATE_NO_UPDATES");
-            response.patchStringProperty("stage", NO_UPDATES);
-            mqttClient.beginMessage(topic, response.getJson().length(), retained, qos, dup);
-            mqttClient.print(response.getJson());
-            mqttClient.endMessage();
-            break;
-          }
-        case HTTP_UPDATE_OK: {
-            Serial.println("HTTP_UPDATE_OK");
-            response.patchStringProperty("stage", UPDATE_OK);
-            mqttClient.beginMessage(topic, response.getJson().length(), retained, qos, dup);
-            mqttClient.print(response.getJson());
-            mqttClient.endMessage();
-            delay(100);
-            ESP.restart();
-            break;
-          }
-      }
-    }
+  while (!mqttClient.connected()) {
+    connectMQTTBroker();
   }
-
+  handleMQTTMessage(handleESP8266Update);
   // do something else
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
