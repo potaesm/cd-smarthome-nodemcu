886ac03e180cbaf426faa71413e560d3d442bc27
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2022-12-07 22:46:27 +0530

    Add DS18B20 temperature sensor

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -1,5 +1,11 @@
 #define DEVMODE
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define NO_SENSOR 0
+#define TEMPERATURE_SENSOR 1
+
 #include <PubSubClient.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
@@ -13,11 +19,12 @@ PubSubClient mqttClient(MQTT_BROKER, MQTT_PORT, wifiClient);
 
 #include "OTAUpdateHelper.h"
 
-bool blinkStatus = false;
+byte sensor = NO_SENSOR;
 const long interval = 1000;
 unsigned long previousMillis = 0;
 
-void handleESP8266Update(String commit, String url) {
+void handleESP8266Update(String commit, String url)
+{
   String response = "{}";
   // Send BIN_URL_RECEIVED
   response = addProperty(response, "id", DEVICE_ID);
@@ -34,39 +41,46 @@ void handleESP8266Update(String commit, String url) {
   t_httpUpdate_return ESPHttpUpdateReturn = ESPhttpUpdate.update(wifiClient, url);
   // Reconnect MQTT and send result
   connectMQTTBroker(softwareReset);
-  switch (ESPHttpUpdateReturn) {
-    case HTTP_UPDATE_FAILED: {
-        // Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
-        response = updateProperty(response, "stage", String(UPDATE_FAILED) + " (" + String(ESPhttpUpdate.getLastError()) + "): " + ESPhttpUpdate.getLastErrorString());
-        sendMQTTMessage(response);
-        break;
-      }
-    case HTTP_UPDATE_NO_UPDATES: {
-        // Serial.println("HTTP_UPDATE_NO_UPDATES");
-        response = updateProperty(response, "stage", NO_UPDATES);
-        sendMQTTMessage(response);
-        break;
-      }
-    case HTTP_UPDATE_OK: {
-        // Serial.println("HTTP_UPDATE_OK");
-        response = updateProperty(response, "stage", UPDATE_OK);
-        sendMQTTMessage(response);
-        break;
-      }
+  switch (ESPHttpUpdateReturn)
+  {
+  case HTTP_UPDATE_FAILED:
+  {
+    // Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+    response = updateProperty(response, "stage", String(UPDATE_FAILED) + " (" + String(ESPhttpUpdate.getLastError()) + "): " + ESPhttpUpdate.getLastErrorString());
+    sendMQTTMessage(response);
+    break;
+  }
+  case HTTP_UPDATE_NO_UPDATES:
+  {
+    // Serial.println("HTTP_UPDATE_NO_UPDATES");
+    response = updateProperty(response, "stage", NO_UPDATES);
+    sendMQTTMessage(response);
+    break;
+  }
+  case HTTP_UPDATE_OK:
+  {
+    // Serial.println("HTTP_UPDATE_OK");
+    response = updateProperty(response, "stage", UPDATE_OK);
+    sendMQTTMessage(response);
+    break;
+  }
   }
   softwareReset();
 }
 
-void mqttPayloadProcess(char* topic, byte* payload, unsigned int payloadLength) {
+void mqttPayloadProcess(char *topic, byte *payload, unsigned int payloadLength)
+{
   handleMQTTUpdateMessage(handleESP8266Update, topic, payload, payloadLength);
 }
 
-void softwareReset() {
+void softwareReset()
+{
   delay(100);
   ESP.restart();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   connectWifi();
   // mqttClient.setServer(MQTT_BROKER, MQTT_PORT);
@@ -74,17 +88,40 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  if (!mqttClient.connected()) {
+void loop()
+{
+  if (!mqttClient.connected())
+  {
     connectMQTTBroker(softwareReset);
   }
   mqttClient.loop();
 
-  // do something else
+  // Main tasks
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= interval)
+  {
     previousMillis = currentMillis;
-    blinkStatus = !blinkStatus;
-    digitalWrite(LED_BUILTIN, blinkStatus);
+    sensor++;
+    switch (sensor)
+    {
+    case TEMPERATURE_SENSOR:
+    {
+      // GPIO where the DS18B20 is connected to
+      OneWire oneWire(D3);
+      DallasTemperature DS18B20(&oneWire);
+      DS18B20.begin();
+      DS18B20.requestTemperatures();
+      float temperatureC = DS18B20.getTempCByIndex(0);
+      // float temperatureF = DS18B20.getTempFByIndex(0);
+      Serial.print(temperatureC);
+      Serial.println("ºC");
+      // Serial.print(temperatureF);
+      // Serial.println("ºF");
+      break;
+    }
+    default:
+      sensor = NO_SENSOR;
+      break;
+    }
   }
 }
